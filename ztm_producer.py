5a168aebb2abe60883b7e61e111c14523bdcf4c2
daulet <shabakovdaulet@gmail.com>
2026-07-10 19:58:58 +0200

    con

--- ztm_producer.py
+++ ztm_producer.py
@@ -9,17 +9,21 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 producer = Kafkaproducer(
-    bootstrap_servers = 'localhost:9092'
+    bootstrap_servers = 'localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-value_serializer=lambda v: json.dumps(v).encode('utf-8')
+
 
 url = "https://api.um.warszawa.pl/api/action/busestrams_get/"
 
 params = {
-    "resource_id"
-    "apikey"
-    "type"
+    "resource_id":"f2e5503e-927d-4ad3-9500-4ab9e55eba59",
+    "apikey": API_KEY,
+    "type":"1"
 }
 
-response 
+print("working with API")
+response = requests.get(url, params=params)
+data = response.json()
+
