404d387db67345dea75ebb22252b7513ac453364
daulet <shabakovdaulet@gmail.com>
2026-07-15 21:03:19 +0200

    o

--- ztm_consumer.py
+++ ztm_consumer.py
@@ -4,3 +4,9 @@ from kafka import KafkaConsumer
 
 conn = psycopg2.connect(dbname = 'airflow', user = 'airflow', host = 'localhost', password = 'airflow')
 
+cursor = conn.cursor()
+consumer = KafkaConsumer(
+    'ztm_transport',
+    bootstrap_servers='localhost:9092',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
