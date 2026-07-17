4a6b1e30591cfe8a894763496c044f2faa07c020
daulet <shabakovdaulet@gmail.com>
2026-07-18 00:44:32 +0200

    f

--- ztm_consumer.py
+++ ztm_consumer.py
@@ -2,7 +2,7 @@ import json
 import psycopg2
 from kafka import KafkaConsumer
 
-conn = psycopg2.connect(dbname = 'airflow', user = 'airflow', host = 'localhost', password = 'airflow')
+
 
 cursor = conn.cursor()
 consumer = KafkaConsumer(
