11661ac8690236e2d9010347082bbea8c6f0acf0
daulet <shabakovdaulet@gmail.com>
2026-07-17 22:01:04 +0200

    created table in db

--- ztm_consumer.py
+++ ztm_consumer.py
@@ -10,3 +10,23 @@ consumer = KafkaConsumer(
     bootstrap_servers='localhost:9092',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+for message in consumer:
+    print("receiving data")
+    data = message.value
+    lines = data.get('Lines')
+    lon = data.get('Lon')
+    VehicleNumber = data.get('VehicleNumber')
+    Time = data.get('Time')
+    Lat = data.get('Lat')
+    Brigade = data.get('Brigade')
+    print(data)
+
+    insert_query = """
+        INSERT INTO bus_data (vehicle_number, line, latitude, longitude, bus_time)
+        VALUES (%s, %s, %s, %s, %s)
+    """
+
+    cursor.execute(insert_query, (VehicleNumber, lines, Lat, lon, Time))
+
+    conn.commit()
